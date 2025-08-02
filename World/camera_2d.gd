dacbfe49408343c43482b17b544dbf83b70f38f1
kataphrakt0s <cam.m.muir@gmail.com>
2025-08-01 19:07:07 -0600

    initial commit

---
+++ World/camera_2d.gd
@@ -0,0 +1,11 @@
+extends Camera2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	zoom = GlobalSettings.ZOOM_FACTOR
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
