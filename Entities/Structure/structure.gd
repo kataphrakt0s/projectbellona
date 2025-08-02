a6907ad7c8122b1288ff66b3c834728b9e39d8fa
kataphrakt0s <cam.m.muir@gmail.com>
2025-08-02 15:28:40 -0600

    added basic structure capture

--- Entities/Structure/structure.gd
+++ Entities/Structure/structure.gd
@@ -1,10 +1,14 @@
 class_name Structure extends Area2D
 
+signal captured(team: EntityManager.TEAMS)
 
 @export var team: EntityManager.TEAMS = EntityManager.TEAMS.NEUTRAL
 @export var structure_type: EntityManager.STRUCTURES = EntityManager.STRUCTURES.FORT
 
+var capturing_team: EntityManager.TEAMS = EntityManager.TEAMS.NEUTRAL
+
 @onready var structure_sprite := $StructureSprite
+@onready var capture_timer := $CaptureTimer
 
 func _ready() -> void:
 	self.add_to_group("Structures")
@@ -15,6 +19,36 @@ func _ready() -> void:
 			{"team": get_enum_name(EntityManager.TEAMS, team).to_lower(), "type": get_enum_name(EntityManager.STRUCTURES, structure_type).to_lower()}
 		)
 	)
+
+func start_capture(team: EntityManager.TEAMS):
+	capture_timer.start(5)
+	capturing_team = team
+
+func stop_capture():
+	capturing_team = EntityManager.TEAMS.NEUTRAL
+	capture_timer.stop()
+
+func _on_capture_timer_timeout() -> void:
+	team = capturing_team
+	captured.emit(capturing_team)
+	
+	# Update sprite
+	structure_sprite.texture = load(
+		"res://Entities/Structure/Resources/{team}/{team}_{type}.tres".format(
+			{"team": get_enum_name(EntityManager.TEAMS, team).to_lower(), "type": get_enum_name(EntityManager.STRUCTURES, structure_type).to_lower()}
+		)
+	)
+	
+	print("Structure was captured by {team}".format({"team": get_enum_name(EntityManager.TEAMS, team)}))
+	
+
+func _on_area_entered(area: Area2D) -> void:
+	if area.is_in_group("Units"):
+		start_capture(area.team)
+
+func _on_area_exited(area: Area2D) -> void:
+	if area.is_in_group("Units"):
+		stop_capture()
 
 # Helpers
 func get_enum_name(enum_dict: Dictionary, value: int) -> String:
